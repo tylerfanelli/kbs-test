1b0d8a66c94760a7bf17c862636e8a329703b985
Tyler Fanelli <tfanelli@redhat.com>
2024-12-06 21:57:28 -0500

    add ECDH support

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{io, sync::RwLock};
+use std::{cmp::min, collections::BTreeMap, io, sync::RwLock};
 
 use actix_web::{cookie::Cookie, post, web, App, HttpRequest, HttpResponse, HttpServer};
-use base64::prelude::*;
-use kbs_types::{Challenge, Request, Response, TeePubKey};
-use lazy_static::lazy_static;
-use openssl::{
-    bn::BigNum,
-    pkey::Public,
-    rsa::{Padding, Rsa},
+use aes::{
+    cipher::{BlockEncrypt, KeyInit},
+    Aes128,
 };
+use base64::prelude::*;
+use elliptic_curve::JwkEcKey;
+use kbs_types::{Challenge, ProtectedHeader, Request, Response, TeePubKey};
+use lazy_static::lazy_static;
+use p384::{ecdh::EphemeralSecret, EncodedPoint, NistP384};
+use rand_core::OsRng;
 use serde_json::Value;
+use sha2::Sha256;
 use uuid::Uuid;
 
 lazy_static! {
-    pub static ref KEY: RwLock<Vec<Rsa<Public>>> = RwLock::new(Vec::new());
+    pub static ref KEY: RwLock<Vec<(JwkEcKey, EphemeralSecret)>> = RwLock::new(Vec::new());
 }
 
 #[actix_web::main]
@@ -48,41 +51,37 @@ pub async fn auth(_req: web::Json<Request>) -> HttpResponse {
 /// Placeholder for attesting a client's TEE evidence.
 #[post("/attest")]
 pub async fn attest(req: HttpRequest, attest: web::Json<kbs_types::Attestation>) -> HttpResponse {
-    let cookie = req.cookie("kbs-session-id").unwrap();
-
-    println!("session ID: {}", cookie.value());
+    let _cookie = req.cookie("kbs-session-id").unwrap();
 
     let attest = attest.into_inner();
 
-    let rsa = match attest.tee_pubkey {
-        TeePubKey::RSA {
+    let ec = match attest.tee_pubkey {
+        TeePubKey::EC {
+            crv: _,
             alg: _,
-            k_mod,
-            k_exp,
+            x,
+            y,
         } => {
-            let n_bytes = BASE64_URL_SAFE.decode(&k_mod).unwrap();
-            let e_bytes = BASE64_URL_SAFE.decode(&k_exp).unwrap();
+            let x = BASE64_URL_SAFE.decode(x).unwrap();
+            let y = BASE64_URL_SAFE.decode(y).unwrap();
 
-            let n = BigNum::from_slice(&n_bytes).unwrap();
-            let e = BigNum::from_slice(&e_bytes).unwrap();
+            let epoint = EncodedPoint::from_affine_coordinates(
+                x.as_slice().into(),
+                y.as_slice().into(),
+                false,
+            );
+            let jwk = JwkEcKey::from_encoded_point::<NistP384>(&epoint).unwrap();
+            let private = EphemeralSecret::random(&mut OsRng);
 
-            Rsa::from_public_components(n, e).unwrap()
+            (jwk, private)
         }
         _ => panic!("invalid RSA key"),
     };
 
     let mut key = KEY.write().unwrap();
-    key.push(rsa);
+    key.push(ec);
 
-    let resp = Response {
-        protected: "".to_string(),
-        encrypted_key: "".to_string(),
-        iv: "".to_string(),
-        ciphertext: "".to_string(),
-        tag: "".to_string(),
-    };
-
-    HttpResponse::Ok().json(resp)
+    HttpResponse::Ok().into()
 }
 
 #[post("/{resouce_id}")]
@@ -92,25 +91,51 @@ pub async fn resource(_req: HttpRequest, resource_id: web::Path<String>) -> Http
         panic!("invalid resource id");
     }
 
-    let key = {
+    let (jwk, private) = {
         let mut vec = KEY.write().unwrap();
         vec.pop().unwrap()
     };
 
-    let secret = "hello, SVSM!".to_string();
-    let mut buf = vec![0; key.size() as usize];
-    let len = key
-        .public_encrypt(secret.as_bytes(), &mut buf, Padding::PKCS1)
-        .unwrap();
+    let public = jwk.to_public_key().unwrap();
+    let shared = private.diffie_hellman(&public);
+    let hkdf = shared.extract::<Sha256>(None);
 
-    let encrypted = BASE64_STANDARD.encode(&buf[..len]);
+    let mut out = [0u8; 16];
+    let empty: [u8; 0] = [];
+
+    hkdf.expand(&empty, &mut out).unwrap();
+    let aes = Aes128::new_from_slice(&out).unwrap();
+
+    let plaintext = String::from("hello, SVSM! This message is from the attestation server");
+
+    let mut bytes: Vec<u8> = Vec::new();
+    let mut ptr = 0;
+    let pt_bytes = plaintext.as_bytes();
+    let len = pt_bytes.len();
+
+    while ptr < len {
+        let mut enc = [0u8; 16];
+        let remain = min(16, len - ptr);
+        enc[..remain].copy_from_slice(&pt_bytes[ptr..ptr + remain]);
+
+        aes.encrypt_block((&mut enc).into());
+        bytes.append(&mut enc.to_vec());
+        ptr += 16;
+    }
+
+    let protected = ProtectedHeader {
+        alg: "ECDHP384".to_string(),
+        enc: "AES128".to_string(),
+        other_fields: BTreeMap::new(),
+    };
 
     let resp = Response {
-        protected: "".to_string(),
-        encrypted_key: "".to_string(),
-        iv: "".to_string(),
-        ciphertext: encrypted,
-        tag: "".to_string(),
+        protected,
+        encrypted_key: private.public_key().to_sec1_bytes().to_vec(),
+        aad: None,
+        iv: "".to_string().into(),
+        ciphertext: bytes,
+        tag: "".to_string().into(),
     };
 
     HttpResponse::Ok().json(resp)
